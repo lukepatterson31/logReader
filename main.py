9cb6d06706c6774f773adafb022383ac8cd38971
Luke Patterson <lukepatterson31@gmail.com>
2021-12-06 14:25:20 +0000

    added Node class and requirements

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import pandas as pd
 
+
 def panda_json(file):
     file = pd.read_json(file)
     log_message = file["log"]["Message"]
     return log_message
+
+
+class Node:
+
+    def __init__(self, n, data):
+        self.children = [None]*n
+        self.data = data
+
 
 json_file = "data/sample-corr.json"
 
 if __name__ == '__main__':
     for key in panda_json(json_file):
         print(f"{key}")
+
         value = panda_json(json_file)[key]
         print(value)
-
