550cedaed2f7d9b9238b99862dcd13f66d8c5313
Luke Patterson <lukepatterson31@gmail.com>
2021-12-22 16:15:28 +0000

    implemented basic json traversal

--- logReader/JsonParser.fs
+++ logReader/JsonParser.fs
@@ -4,28 +4,45 @@ open System.IO
 open System.Text.Json
 open System.Text.Json.Nodes
 
+type Node = {
+    Name: string
+    Start: int
+    End: int
+    Duration: int
+    SLA: int
+    Children: Node List
+}
+
 let jsonString = File.ReadAllText "../../../data/sample-corr.json"
-let deserializeJson (json: string): JsonObject = JsonSerializer.Deserialize<JsonObject> json
 
 
 let getLogMessage (jsonStr: string) =
+    let deserializeJson (json: string): JsonObject = JsonSerializer.Deserialize<JsonObject> json
     let getNode (jsonObj: JsonNode, key: string) = jsonObj.Item key
     let root = getNode(getNode(deserializeJson(jsonStr), "log"), "Message")
-    root
+    root.AsObject()
     
-let logRoot = getLogMessage jsonString
+//  Needs work, only need to find:
+//      - SLA
+//      - the nodes that exceed SLA
 
-let rootObj = logRoot.AsObject()
+let rec recursiveDescent (node: JsonNode) =
+        let nodeType = node.GetType()
+        if nodeType = typedefof<JsonObject> then do
+            let child = node.AsObject() 
+            if child.ContainsKey "s" &&
+               child.ContainsKey "e" &&
+               child.ContainsKey "d" then
+                   for v in child do
+                       match v.Key with
+                       | "s" as s -> printfn $"{s}"
+                       | "e" as e -> printfn $"{e}"
+                       | "d" as d -> printfn $"{d}"
+                       |_  -> recursiveDescent v.Value 
+                       
+let rec topLevelDescent (json: JsonObject) =
+    for child in json do
+        child.Value |> recursiveDescent
 
-let rec recursiveDescent (json: JsonObject) =
-    for v in json do
-        let key = v.Key
-        let value = v.Value
-        let vtype = value.GetType()
-        if vtype = typedefof<JsonObject> then do
-            let child = value.AsObject() 
-            if child.ContainsKey "s" ||
-               child.ContainsKey "e" ||
-               child.ContainsKey "d" then printf $"%s{key}: {value}"
-    
-// recursiveDescent rootObj
+// pass in string |> deserialize into json object at level we desire |> recursive descent of nodes
+jsonString |> getLogMessage |> topLevelDescent    
