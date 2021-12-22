3964544ccf959e774ee75d4d8ccd4f856793a605
Luke Patterson <lukepatterson31@gmail.com>
2021-12-22 12:15:13 +0000

    added JsonParser module

--- logReader/Program.fs
+++ logReader/Program.fs
@@ -1,18 +1,40 @@
 namespace logReader
 #nowarn "20"
-open System
-open System.Collections.Generic
 open System.IO
-open System.Linq
-open System.Threading.Tasks
-open Microsoft.AspNetCore
 open Microsoft.AspNetCore.Builder
-open Microsoft.AspNetCore.Hosting
-open Microsoft.AspNetCore.HttpsPolicy
-open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
-open Microsoft.Extensions.Logging
+
+module JsonParser =
+    
+    open System.Text.Json
+    open System.Text.Json.Nodes
+
+    let jsonString = File.ReadAllText "../../../data/sample-corr.json"
+    let deserializeJson (json: string): JsonObject = JsonSerializer.Deserialize<JsonObject> json
+
+
+    let getLogMessage (jsonStr: string) =
+        let getNode (jsonObj: JsonNode, key: string) = jsonObj.Item key
+        let root = getNode(getNode(deserializeJson(jsonStr), "log"), "Message")
+        root
+        
+    let logRoot = getLogMessage jsonString
+
+    let rootObj = logRoot.AsObject()
+
+    let rec recursiveDescent (json: JsonObject) =
+        for v in json do
+            let key = v.Key
+            let value = v.Value
+            let vtype = value.GetType()
+            if vtype = typedefof<JsonObject> then do
+                let child = value.AsObject() 
+                if child.ContainsKey "s" ||
+                   child.ContainsKey "e" ||
+                   child.ContainsKey "d" then printf $"%s{key}: {value}"
+        
+    // recursiveDescent rootObj    
 
 module Program =
     let exitCode = 0
